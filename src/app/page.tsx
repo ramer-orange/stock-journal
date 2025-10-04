d5db30d9da3a1deb5b175a9eb9649d4c8ed4b8d0
ramer-orange <your-email@example.com>
2025-10-04 13:59:04 +0900

    #10 google認証

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,5 @@
 import Image from "next/image";
+import SignIn from "@/components/signIn";
 
 export default function Home() {
   return (
@@ -12,6 +13,7 @@ export default function Home() {
           height={38}
           priority
         />
+        <SignIn />
         <ol className="font-mono list-inside list-decimal text-sm/6 text-center sm:text-left">
           <li className="mb-2 tracking-[-.01em]">
             Get started by editing{" "}
